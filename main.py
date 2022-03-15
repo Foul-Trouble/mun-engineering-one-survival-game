2ee02e540eb64bbcccbd5e130bd4143f43f8ae81
mijmercer <mckennamercer@hotmail.com>
2022-03-15 10:14:55 -0230

    music added

--- main.py
+++ main.py
@@ -1,59 +1,16 @@
-from loads import *
+# This is a sample Python script.
 
-# Testing Stuff
-if __name__ == "__main__":
-    None
-
-
-# Main Loop !!!DO NOT TOUCH, RUSSELL ONLY!!!
-def init():
-    global arduino, clock
-    pygame.display.set_caption('ENGI Survival')
-    screen = pygame.display.set_mode((1000, 720), 0, 32)
-    clock = pygame.time.Clock()
-    try:
-        digital_read(6)
-        arduino = True
-    except Exception:
-        print("Arduino not detected")
-        arduino = False
-    student = Character()
-    teacher = Enemy()
-    coin_summon = Coins()
-    grade_summon = Grades()
-    time_since_enter = pygame.time.get_ticks() - start_time
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
-        if time_since_enter >= 8080:
-            break
-        if arduino:
-            if digital_read(6):
-                break
-        logo_approach = time_since_enter / 5
-        if time_since_enter <= 4040:
-            screen.blit(pygame.transform.scale(logo, (logo_approach, logo_approach / 2)), (96, 135))
-        else:
-            screen.blit(pygame.transform.scale(logo, (808, 404)), (96, 135))
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def close():
-    None
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def loop():
-    main_game()
-    pygame.display.update()
-    clock.tick(60)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-
-init()
-while game:
-    loop()
-
-# Exit Stuff
-close()
-exit()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
